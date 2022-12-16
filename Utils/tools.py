8233b555d6e447753b467ed7d3a74beb53852eb0
Michelle Havenaar <michelle.havenaar2@kpn.com>
2022-12-16 21:54:26 +0100

    day 13 done, huzzah!

--- Utils/tools.py
+++ Utils/tools.py
@@ -111,3 +111,57 @@ def get_dict_key_by_value(val, my_dict):
     for key, value in my_dict.items():
         if val == value:
             return key
+
+
+def merge_sort(arr, left_index, right_index):
+    if left_index >= right_index:
+        return
+    
+    mid = (left_index + right_index) //2
+    merge_sort(arr, left_index, mid)
+    merge_sort(arr, mid +1, right_index)
+    merge(arr, left_index, right_index, mid)
+
+
+def merge(arr, left_index, right_index, middle):
+
+    # Make copies the 2 lists
+    left_copy = arr[left_index:middle + 1]
+    right_copy = arr[middle+1:right_index+1]
+
+    # pointers
+    left_copy_index = 0
+    right_copy_index = 0
+    sorted_index = left_index
+
+    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
+
+        # If our left_copy has the smaller element, put it in the list at the position of the sorted pointer
+        # and update pointers
+        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
+            # print(f"left {left_copy[left_copy_index]} is smaller or equal then right {right_copy[right_copy_index]}")
+            # print(f"insert left at sorted index {sorted_index}")
+            arr[sorted_index] = left_copy[left_copy_index]
+            left_copy_index += 1
+        else:
+            # print(f"left {left_copy[left_copy_index]} is greater or equal then right {right_copy[right_copy_index]}")
+            # print(f"insert right at sorted index {sorted_index}")
+            arr[sorted_index] = right_copy[right_copy_index]
+            right_copy_index += 1
+
+        sorted_index = sorted_index + 1
+
+    # get the remainding elements in the copy lists
+    while left_copy_index < len(left_copy):
+        # print(f"remaining element {left_copy[left_copy_index]}")
+        # print(f"insert at sorted index {sorted_index}")
+        arr[sorted_index] = left_copy[left_copy_index]
+        left_copy_index = left_copy_index + 1
+        sorted_index = sorted_index + 1
+
+    while right_copy_index < len(right_copy):
+        # print(f"remaining element {right_copy[right_copy_index]}")
+        # print(f"insert at sorted index {sorted_index}")
+        arr[sorted_index] = right_copy[right_copy_index]
+        right_copy_index = right_copy_index + 1
+        sorted_index = sorted_index + 1
